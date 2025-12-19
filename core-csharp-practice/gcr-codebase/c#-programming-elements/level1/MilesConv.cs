408d5297539c22634a29e7909f822c9b35ef11d6
Ashita_02 <ashitavarshney37532@gmail.com>
2025-12-19 16:36:13 +0530

    [Ashita] Add . Miles Conversion

---
+++ core-csharp-practice/gcr-codebase/c#-programming-elements/level1/MilesConv.cs
@@ -0,0 +1,12 @@
+using System;
+class MilesConv
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter distance in kilometers:");
+        double km = ConvertToDouble(Console.WriteLine());
+        //formula to convert kilometers to miles 
+        double miles = km / 1.6;
+        Console.WriteLine("The total miles is " + miles + " mile for the given " + km + " km");
+    }
+}
