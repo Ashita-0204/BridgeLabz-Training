49cba76b61d25688bce91f908f16d9c26a0515fb
Ashita_02 <ashitavarshney37532@gmail.com>
2025-12-19 16:35:06 +0530

    [Ashita] Add . Harry's Age Calculation

---
+++ core-csharp-practice/gcr-codebase/c#-programming-elements/level1/Harry.cs
@@ -0,0 +1,12 @@
+using System;
+class Harry
+{
+    static void Main(string[] args)
+    {
+        int birthyear = 2000;
+        int currentyear = 2024;
+        //formula to calculate current age 
+        int age = currentyear - birthyear;
+        Console.WriteLine("Harry's age in 2024 is " + age);
+    }
+}
