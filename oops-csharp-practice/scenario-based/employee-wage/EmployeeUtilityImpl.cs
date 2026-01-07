ca0695a69000b59ac4f092e2d728e57b42130869
Ashita_02 <ashitavarshney37532@gmail.com>
2026-01-07 16:11:17 +0530

    [Ashita] Add . UC-4 Implementation for Wages Management

--- oops-csharp-practice/scenario-based/employee-wage/EmployeeUtilityImpl.cs
+++ oops-csharp-practice/scenario-based/employee-wage/EmployeeUtilityImpl.cs
@@ -17,6 +17,8 @@ namespace Training.employee_wages
             employee.EmpId = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter the employee name ");
             employee.EmpName = Console.ReadLine();
+            Console.WriteLine("Enter Daily Wages");
+            employee.DailyWages = int.Parse(Console.ReadLine());
             return employee;
         }
         //UC 1 Implementation 
@@ -31,6 +33,29 @@ namespace Training.employee_wages
             {
                 Console.WriteLine("Employee is present");
             }
+        }
+        //UC 2+ UC3 +UC4 Implementation
+        public int CalculateDailyWage()
+        {
+            int empType = random.Next(0, 3);
+            int hours = 0;
+
+            switch (empType)
+            {
+                case 1:
+                    hours = Employee.FULL_DAY_HOUR;
+                    break;
+
+                case 2:
+                    hours = Employee.PART_TIME_HOUR;
+                    break;
+
+                default:
+                    hours = 0;
+                    break;
+            }
+
+            return hours * Employee.WAGE_PER_HOUR;
         }
     }
 }
