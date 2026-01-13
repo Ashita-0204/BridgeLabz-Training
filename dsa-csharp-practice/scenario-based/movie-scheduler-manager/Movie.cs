16466fe6feb781494e8af772296bfec6380f91dc
Ashita_02 <ashitavarshney37532@gmail.com>
2026-01-13 16:36:33 +0530

    [Ashita] Add . Scenario based implementation for Movie Scheduler

---
+++ dsa-csharp-practice/scenario-based/movie-scheduler-manager/Movie.cs
@@ -0,0 +1,12 @@
+using System;
+class Movie
+{
+    public string title { get; set; }
+    public string showTime { get; set; }
+
+    public Movie(string title, string showTime)
+    {
+        title = this.title;
+        showTime = this.showTime;
+    }
+}
