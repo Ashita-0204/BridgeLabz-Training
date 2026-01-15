f2ea2d5240f174120003b9a3d3d31ed97847c9d5
Ashita_02 <ashitavarshney37532@gmail.com>
2026-01-16 02:12:00 +0530

    [Ashita] add. Implementation of the Sorting Alphabetically

---
+++ dsa-csharp-practice/scenario-based/address-book-problem/IAddress.cs
@@ -0,0 +1,15 @@
+public interface IAddress
+{
+    void SelectAddressBook();
+    void AddContact();
+    void EditDetail();
+    void DeleteContact();
+    void MultipleUsers();
+    void AddressBook();
+    bool isDuplicate();
+    void SearchAcrossAddressBooks();
+    void ViewByCityOrState();
+    void SearchByCityOrState();
+    void CountByCityOrState();
+    void SortByName();
+}
