15f4a42fd4049feb63480c595c5b86cc3ce1892e
Ashita_02 <ashitavarshney37532@gmail.com>
2026-01-16 00:15:49 +0530

    [Ashita] Add . Implementation for setting the encapsulated datafields

---
+++ dsa-csharp-practice/scenario-based/address-book-problem/User.cs
@@ -0,0 +1,50 @@
+class User
+{
+    //UC-1 Implementation for setting the attributes fields
+    private string firstName;
+    private string lastName;
+    private string address;
+    private string city;
+    private string state;
+    private string zip;
+    private string email;
+    private int phoneNum;
+    //Encapsulation for data security
+    public string FirstName
+    {
+        get => firstName; set => firstName = value;
+    }
+    public string LastName
+    {
+        get => lastName; set => lastName = value;
+    }
+    public string Address
+    {
+        get => address; set => address = value;
+    }
+    public string City
+    {
+        get => city; set => city = value;
+    }
+    public string State
+    {
+        get => state; set => state = value;
+    }
+    public string Zip
+    {
+        get => zip; set => zip = value;
+    }
+    public string Email
+    {
+        get => email; set => email = value;
+    }
+    public int PhoneNum
+    {
+        get => phoneNum; set => phoneNum => value;
+    }
+    public override string ToString()
+    {
+        return FirstName + " " + LastName + ", " + City + ", " + State + ", " + PhoneNumber;
+    }
+
+}
